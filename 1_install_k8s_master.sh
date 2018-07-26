c28d73bfaf85b6e35fd2ac33007945f1ae488f55
Huabing Zhao <zhaohuabing@gmail.com>
2018-07-26 07:10:40 +0000

    Add sudo to command
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- 1_install_k8s_master.sh
+++ 1_install_k8s_master.sh
@@ -50,7 +50,7 @@ case "$choice" in
     wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz 
     tar -zxvf helm-v2.9.1-linux-amd64.tar.gz
     chmod o+x linux-amd64/helm
-    mv linux-amd64/helm /usr/local/bin/helm
+    sudo mv linux-amd64/helm /usr/local/bin/helm
     rm -rf linux-amd64
     rm -rf helm-v2.9.1-linux-amd64.tar.gz
 
